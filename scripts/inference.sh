98c53c753b4ca49b36bbbbbeba30210b6cf1341d
dreamer <117450912+aurelianocyp@users.noreply.github.com>
2024-01-10 20:54:35 +0800

    Update inference.sh

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=0
 python -m torch.distributed.launch --nproc_per_node=1 --master_port 12345 inference_refine_1D_cam.py \
 --config ./config/otavatar.yaml \
 --name otavatar \
